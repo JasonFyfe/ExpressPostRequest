c6f4568385bb912f43e84e9e564eaa0640968129
j_fyfe <jasonfyfe.dev@gmail.com>
2020-09-09 12:26:46 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var port = 4000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.post("/addfriend", function(req, res){
+    var newFriend = req.body.newfriend;
+    console.log(req.body.newfriend);
+    friends.push(newFriend);
+    res.redirect("/friends");
+});
+
+app.get("/friends", function(req, res){
+    res.render("friends", {friends: friends});
+});
+
+app.listen(port, function(){
+    console.log(`Server listening on port: ${port}`);
+});
